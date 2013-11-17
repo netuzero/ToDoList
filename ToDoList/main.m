315e3b7569c6b46d5790660b05eda150ef5e6252
netuzero <nirvanacn@live.com>
2013-11-17 13:22:38 +0800

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by uzero on 13-11-17.
+//  Copyright (c) 2013年 net.uzero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
